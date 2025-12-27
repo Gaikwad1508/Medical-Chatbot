6ec254b0b76187ef4c1bf941fb85c3e5374e847c
Abhishek <gaikwadabhishek1508@gmial.com>
2025-12-27 12:44:36 +0530

    Add the project structure by creating folders and files

---
+++ template.sh
@@ -0,0 +1,15 @@
+#Create the directories
+mkdir -p src
+mkdir -p research
+
+#Create the files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "All directories and files were created successfully..."
