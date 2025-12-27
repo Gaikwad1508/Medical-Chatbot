bad617a28fd4ccf30ad5c126770426e2c611c99e
Abhishek <gaikwadabhishek1508@gmial.com>
2025-12-27 22:06:36 +0530

    Add the requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical-Chatbot",
+    version = "0.1.0",
+    author = "Abhishek Santosh Gaikwad"
+    author_email = "gaikwadabhishek1508@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
